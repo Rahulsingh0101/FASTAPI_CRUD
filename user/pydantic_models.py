12286323107df5dcf34c70199b9f86c3c898e63d
rahulsingh0101 <rahulsinghbusiness0101@gmail.com>
2023-01-05 17:04:23 +0530

    fastapi_crud

---
+++ user/pydantic_models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+import uuid
+
+
+
+class createuser(BaseModel):
+    name : str
+    email : str
+    password :str
+   
+
+class loginuser(BaseModel):
+    username : str
+    password : str
+    
+    
+class updateuser(BaseModel):
+    id :uuid.UUID
+    name : str
+    email : str
+
+class deleteuser(BaseModel):
+    email : str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
